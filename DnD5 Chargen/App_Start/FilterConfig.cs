6f2db37e022d33be02b8b84184c38cc67e75f826
Jaderade <gaydaradejade@gmail.com>
2017-09-23 18:17:35 -0400

    Add project files.

---
+++ DnD5 Chargen/App_Start/FilterConfig.cs
@@ -0,0 +1,10 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace DnD5_Chargen {
+  public class FilterConfig {
+    public static void RegisterGlobalFilters(GlobalFilterCollection filters) {
+      filters.Add(new HandleErrorAttribute());
+    }
+  }
+}
---
+++ DnD5 Chargen/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DnD5_Chargen.Startup))]
+namespace DnD5_Chargen
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
